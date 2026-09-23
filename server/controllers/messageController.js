9a667b767b00da6c8d44bdb6177166def3f1d08b
Kiran Kumar <1kiran1234567990@gmail.com>
2026-09-24 00:20:53 +0530

    Update Groq model

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -37,7 +37,7 @@ export const textMessageController = async (req, res) => {
         chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false})
 
         const { choices } = await openai.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
+        model: "openai/gpt-oss-120b",
         messages: [
             {
                 role: "user",
